d23b9dd1a6f15d41f743a7a078a45225ea19b03a
gracey12051@gmail.com <gracey12051@gmail.com>
2018-04-15 19:52:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {Slowdive: 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"}
 
-function updatePlaylist (playlist, artistName, songTitle){
+function updatePlaylist (playlist, artistName, songTitle)
+{
   playlist.artistName = songTitle
   return playlist
 }
