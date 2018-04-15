00423df0f21ec08cf4cfa222f91922326b9c0c42
gracey12051@gmail.com <gracey12051@gmail.com>
2018-04-15 19:51:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {Slowdive: 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"}
 
 function updatedPlaylist (playlist, artistName, songTitl){
-  obj[key] = value
+  playlist.artistName = 
   return obj
 }
 
